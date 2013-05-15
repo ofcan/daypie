0310d9bc0158a440594eb50311b67290cb4eb08a
kira <sven.dupli@gmail.com>
2013-05-15 20:21:57 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BareboneRails::Application.config.secret_token = '8154225186aec7ce7e6f74926ea120cc8a3a1e9984f940f39318efb25f14a55cbeead8557ef1bba48aaf0410b0564f7012a3020aac63857059252add339785af'
